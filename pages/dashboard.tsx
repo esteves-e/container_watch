65ddf923d3bebe5b15e7ef09b2b696189d31eb88
esteves-e <eduu.esteves@hotmail.com>
2025-04-25 00:08:41 -0300

    Fix: Alteração para tentar qrcode

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -36,7 +36,7 @@ export default function Dashboard() {
   const router = useRouter()
   const [userEmail, setUserEmail] = useState<string | null>(null)
 
-  const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+  const baseURL = process.env.NEXT_PUBLIC_BASE_URL || (typeof window !== 'undefined' ? window.location.origin : '')
 
   useEffect(() => {
     const email = localStorage.getItem('email')
