eee40c77714ddef007b1e1a7914d5f4bd4dbb669
esteves-e <eduu.esteves@hotmail.com>
2025-04-26 14:25:20 -0300

    att o _app.tsx para utilizar o title global

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 import 'nprogress/nprogress.css'
 import type { AppProps } from 'next/app'
@@ -21,7 +22,6 @@ export default function App({ Component, pageProps }: AppProps) {
     }
 
     const handleStop = () => {
-      // Garante que o NProgress dure no mínimo 400ms
       timer = setTimeout(() => {
         NProgress.done()
       }, 0)
@@ -44,7 +44,12 @@ export default function App({ Component, pageProps }: AppProps) {
       supabaseClient={supabaseClient}
       initialSession={pageProps.initialSession}
     >
+      <Head>
+        <title>ContainerWatch</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="Plataforma de gestão e inspeção de containers, veículos e embarcações." />
+      </Head>
       <Component {...pageProps} />
     </SessionContextProvider>
   )
-}
+} 
